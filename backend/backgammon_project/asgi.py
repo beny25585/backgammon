bb531fca2643cbfbf134b493631566a7bc86f894
beny <binyamin25585@gmail.com>
2026-07-27 13:37:19 +0300

    Fix asgi.py import order - load Django before importing consumers

--- backend/backgammon_project/asgi.py
+++ backend/backgammon_project/asgi.py
@@ -1,13 +1,14 @@
 import os
 from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
-from channels.security.websocket import AllowedHostsOriginValidator
-import game.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backgammon_project.settings')
 
 django_asgi_app = get_asgi_application()
+
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+from channels.security.websocket import AllowedHostsOriginValidator
+import game.routing
 
 application = ProtocolTypeRouter({
     'http': django_asgi_app,
